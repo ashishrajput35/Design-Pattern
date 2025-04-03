04477aa46cfc12ad7fa80ca4cf2fd7d29cfd0905
ashishrajput35 <ashishrajput35cs@gmail.com>
2025-04-03 23:56:08 +0530

    Initail commit

---
+++ singletonPattern/SingletonPattern.java
@@ -0,0 +1,30 @@
+class SingletonPatternTest{
+
+    private static volatile SingletonPatternTest singletonPatternTest;
+
+    private SingletonPatternTest(){}
+
+    public static SingletonPatternTest getIntaceSingletonPatternTest(){
+
+        if(singletonPatternTest == null ){
+
+            return new SingletonPatternTest();
+        }
+
+        return singletonPatternTest;
+    }
+
+}
+
+public class SingletonPattern{
+
+    public static void main (String[] args){
+
+        System.out.println("This is pattern");
+
+
+    }
+}
+
+
+
